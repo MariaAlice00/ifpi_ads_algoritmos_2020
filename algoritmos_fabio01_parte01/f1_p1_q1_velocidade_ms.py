4a1dd83c6bfaeb59de02f3320241c45b50523356
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-08-13 21:13:15 -0300

    Create f1_p1_q1_velocidade_ms.py

---
+++ algoritmos_fabio01_parte01/f1_p1_q1_velocidade_ms.py
@@ -0,0 +1,11 @@
+# Leia uma velocidade em m/s, calcule e escreva esta velocidade em km/h
+# Vkm/h = Vm/s * 3.6
+
+# Entrada
+velocidade_ms = int(input('Digite a velocidade em m/s: '))
+
+# Processamento
+velocidade_km = velocidade_ms * 3.6
+
+# Saída
+print(f'{velocidade_ms} m/s é igual a {velocidade_km} km/h')
