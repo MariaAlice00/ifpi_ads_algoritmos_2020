e622afa477d51332d1b64c3c8cd7826b4dbebee4
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-08-13 21:15:22 -0300

    Create f1_p2_q1_metros_centimetros.py

---
+++ algoritmos_fabio01_parte02/f1_p2_q1_metros_centimetros.py
@@ -0,0 +1,10 @@
+# Leia um valor em m, calcule e escreva o equivalente em cm.
+
+# Entrada
+metro = float(input('Digite um valor: '))
+
+# Processamento
+centimetro = metro * 100 
+
+# Saída
+print('{} metros equivalem a {} centímetros'. format(metro, centimetro))
