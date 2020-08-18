aab24bd80a7c08bd8075b549ee530b9d36b776d2
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-08-18 20:32:34 -0300

    Update 06.py

--- algoritmos_fabio_while/06.py
+++ algoritmos_fabio_while/06.py
@@ -1,10 +1,10 @@
+# Escreva a tabuada dos números de 1 a 10.
+
 num = int(input('>>> '))
 primeiro = 1
 ultimo = 10
 x = primeiro
+
 while x <= ultimo:
     print('{} x {:2} = {:2}'.format(num, x, x*num))
     x += 1
-
-
-
