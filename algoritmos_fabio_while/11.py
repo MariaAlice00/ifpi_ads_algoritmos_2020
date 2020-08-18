7a40215b6fd6c84825447660cdeb68ed1d2be4c5
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-08-18 19:58:01 -0300

    Add files via upload

---
+++ algoritmos_fabio_while/11.py
@@ -0,0 +1,17 @@
+primeiro = int(input('Primeiro: '))
+ultimo = int(input('Último: '))
+
+contador = 1
+x = 0
+while primeiro < ultimo:
+    y = primeiro - 1
+    while y > 1:
+        if primeiro % y == 0:
+            break
+        y = y - 1
+        contador = contador + 1
+    else:
+        print(primeiro, end=' ')
+        x = x + 1
+    primeiro = primeiro + 1
+
---
+++ algoritmos_fabio_while/15.py
@@ -0,0 +1,16 @@
+# Leia N, calcule e escreva os N primeiros termos de seqüência (1, 3, 6, 10, 15,...)
+
+n = int(input('>>> '))
+y = 1
+x = 2
+contador = 1
+p = 1
+print('{}'.format(1), end=' ')
+
+while True:
+    y = y + x
+    x = x + 1
+    contador = contador + 1
+    if contador > n:
+        break
+    print('-> {}'.format(y), end=' ')
