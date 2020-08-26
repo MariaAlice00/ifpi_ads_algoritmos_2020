954650347f1799dc3f761c2114910eb1e3b54646
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-08-26 17:36:36 -0300

    Update 09.py

--- algoritmos_fabio_while/09.py
+++ algoritmos_fabio_while/09.py
@@ -1,9 +1,16 @@
 '''Leia LimiteSuperior e LimiteInferior e escreva todos os números pares entre os limites lidos.'''
-lim_sup = int(input('Limite superior: '))
-lim_inf = int(input('Limite inferior: '))
 
-x = lim_inf
-while x <= lim_sup:
-    if x % 2 == 0:
-        print(x)
-    x += 1
+def main():
+    lim_inf = int(input('Primeiro número: '))
+    lim_sup = int(input('Último número: '))
+
+    print('Os números pares entre {} e {} são:'.format(lim_inf, lim_sup))
+
+    x = lim_inf
+
+    while x <= lim_sup:
+        if x % 2 == 0:
+            print(x)
+        x += 1
+
+main()
