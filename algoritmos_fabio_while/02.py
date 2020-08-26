2df32833f0357fa1f4484eb789acb7783666a538
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-08-26 17:13:23 -0300

    Update 02.py

--- algoritmos_fabio_while/02.py
+++ algoritmos_fabio_while/02.py
@@ -1,9 +1,12 @@
 # Leia N e escreva todos os números inteiros pares de 1 a N.
 
-n = int(input('>>> '))
-x = 1
-while x <= n:
-    if x % 2 == 0:
-        print(x)
-    x += 1
-print('Fim...')
+def main():
+    n = int(input('>>> '))
+    x = 1
+    while x <= n:
+        if x % 2 == 0:
+            print(x)
+        x += 1
+    print('Fim...')
+
+main()
