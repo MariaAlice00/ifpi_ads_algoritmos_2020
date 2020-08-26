de78d178dc0a7dec79fa30a565b0eb680794285e
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-08-26 17:20:45 -0300

    Update 05.py

--- algoritmos_fabio_while/05.py
+++ algoritmos_fabio_while/05.py
@@ -1,9 +1,15 @@
 # Leia um número, calcule e escreva seu fatorial.
 
-num = int(input('>>> '))
-total = 1
-contador = 1
-while contador <= num:
-    total = total * contador
-    contador = contador + 1
-print(total)
+def main():
+    num = int(input('>>> '))
+
+    total = 1
+    contador = 1
+    
+    while contador <= num:
+        total = total * contador
+        contador = contador + 1
+        
+    print(total)
+
+main()
