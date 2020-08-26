3f86cab5b7fa683c2007a89c317a1a915b6c3ff1
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-08-26 17:47:08 -0300

    Update 12.py

--- algoritmos_fabio_while/12.py
+++ algoritmos_fabio_while/12.py
@@ -1,18 +1,23 @@
 # Leia N e uma lista de N números e escreva a soma e a média de todos os números da lista.
 
-from random import randint
-n = int(input('>>> '))
-contador = 1
-soma = 0
+def main():
+    from random import randint
 
-while True:
-    num = randint(0,10)
-    contador = contador + 1
-    if contador > n + 1:
-        break
+    n = int(input('>>> '))
+    contador = 1
+    soma = 0
 
-    print(num, end=' ')
-    soma = soma + num
+    while True:
+        num = randint(0,10)
+        contador = contador + 1
+        if contador > n + 1:
+            break
 
-print('\nSoma = {}'.format(soma))
-print('Média = {}'.format(soma / n))
+        print(num, end=' ')
+
+        soma = soma + num
+
+    print('\nSoma = {}'.format(soma))
+    print('Média = {}'.format(soma / n))
+
+main()
