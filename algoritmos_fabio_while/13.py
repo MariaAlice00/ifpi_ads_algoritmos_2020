5402673f53ee02e8d239385e7710dddd258f20a0
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-08-26 17:48:56 -0300

    Update 13.py

--- algoritmos_fabio_while/13.py
+++ algoritmos_fabio_while/13.py
@@ -1,17 +1,21 @@
 # Leia N e uma lista de N números e escreva o maior número da lista.
-from random import randint
 
-n = int(input('>>> '))
-contador = 1
-lista = []
+def main():
+    from random import randint
 
-while True:
-    num = randint(0,10)
-    contador = contador + 1
-    if contador > n + 1:
-        break
-    
-    lista.append(num) # append: adiciona um item na lista
-    print(num, end=' ')
+    n = int(input('>>> '))
+    contador = 1
+    lista = []
 
-print('\nMaior número: {}'.format(max(lista)))
+    while True:
+        num = randint(0,10)
+        contador = contador + 1
+        if contador > n + 1:
+            break
+        
+        lista.append(num) # append: adiciona um item na lista
+        print(num, end=' ')
+
+    print('\nMaior número: {}'.format(max(lista)))
+
+main()
