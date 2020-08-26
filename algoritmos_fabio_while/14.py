19335c931c8b3161ea139ed84e8143f2cf7bbefa
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-08-26 17:50:05 -0300

    Update 14.py

--- algoritmos_fabio_while/14.py
+++ algoritmos_fabio_while/14.py
@@ -1,15 +1,19 @@
 # Leia N, calcule e escreva o maior quadrado menor ou igual a N. Por exemplo, se N for igual a 38, o maior quadrado menor que 38 é 36 (quadrado de 6).
-n = int(input('>>> '))
 
-x = 1
-lista = []
+def main():
+    n = int(input('>>> '))
 
-while True:
-    y = x * x
-    x = x + 1
-    if y > n:
-        break
-    
-    lista.append(y)
+    x = 1
+    lista = []
 
-print(max(lista))
+    while True:
+        y = x * x
+        x = x + 1
+        if y > n:
+            break
+        
+        lista.append(y)
+
+    print(max(lista))
+
+main()
