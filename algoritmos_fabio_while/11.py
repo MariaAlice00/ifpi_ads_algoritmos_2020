44f9d3139cb9d30c8c415230eb577d2676eaa4bd
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-08-26 17:41:45 -0300

    Update 11.py

--- algoritmos_fabio_while/11.py
+++ algoritmos_fabio_while/11.py
@@ -1,18 +1,24 @@
-# Leia LimiteSuperior e LimiteInferior e escreva todos os números primos entre os limites lidos.
+'''Leia LimiteSuperior e LimiteInferior e escreva todos os números primos entre os limites lidos.'''
 
-primeiro = int(input('Primeiro: '))
-ultimo = int(input('Último: '))
+def main():
+    primeiro = int(input('Primeiro: '))
+    ultimo = int(input('Último: '))
 
-contador = 1
-x = 0
-while primeiro < ultimo:
-    y = primeiro - 1
-    while y > 1:
-        if primeiro % y == 0:
-            break
-        y = y - 1
-        contador = contador + 1
-    else:
-        print(primeiro, end=' ')
-        x = x + 1
-    primeiro = primeiro + 1
+    contador = 1
+    x = 0
+    
+    while primeiro < ultimo:
+        y = primeiro - 1
+        
+        while y > 1:
+            if primeiro % y == 0:
+                break
+            y = y - 1
+            contador = contador + 1
+        else:
+            print(primeiro, end=' ')
+            x = x + 1
+        
+        primeiro = primeiro + 1
+
+main()
