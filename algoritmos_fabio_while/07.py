623a09a9cdf03ca93675a48179d86a14354a74e9
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-08-26 17:27:47 -0300

    Update 07.py

--- algoritmos_fabio_while/07.py
+++ algoritmos_fabio_while/07.py
@@ -1,9 +1,15 @@
 '''Leia um número N, some todos os números inteiros entre 1 e N e escreva o resultado obtido.'''
 
-n = int(input('>>> '))
-total = 0
-contador = 1
-while contador <= n:
-    total = total + contador
-    contador = contador + 1
-print(total)
+def main():
+    n = int(input('>>> '))
+
+    total = 0
+    contador = 1
+    
+    while contador <= n:
+        total = total + contador
+        contador = contador + 1
+    
+    print(total)
+
+main()
