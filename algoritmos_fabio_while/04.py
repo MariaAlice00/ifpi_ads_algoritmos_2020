e76abb0dd6f689d445246d7e08d1f31ad732d2e1
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-09-01 10:50:24 -0300

    Update 04.py

--- algoritmos_fabio_while/04.py
+++ algoritmos_fabio_while/04.py
@@ -1,14 +1,13 @@
 # Leia as variáveis A0, Limite e R e escreva os valores menores que Limite gerados pela Progressão Geométrica que tem por valor inicial A0 e razão R.
 
 def main():
-    a0 = int(input('Primeiro termo: '))
-    limite = int(input('Limite: '))
-    razao = int(input('Razão: '))
+    A0 = int(input('Primeiro termo: '))
+    limite = int(input('Último número: '))
+    r = int(input('Razão: '))
 
-    while a0 < limite:
-        a0 = a0 * razao
-        if a0 >= limite:
-            break
-        print(a0)
+    p = A0
+    while p < limite:
+        print(p)
+        p *= r
 
 main()
