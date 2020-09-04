52ea059cc8a1a3c597a65b3f3b2680d08bb2d28e
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-09-03 23:04:26 -0300

    Delete 06.py

--- fabio_for/06.py
+++
@@ -1,5 +0,0 @@
-'''Escreva a tabuada dos números de 1 a 10.'''
-
-num = int(input('Você quer saber a tabuada de que número? '))
-for c in range(1, 11):
-    print('{} x {:2} = {:2}'.format(num, c, num*c))
