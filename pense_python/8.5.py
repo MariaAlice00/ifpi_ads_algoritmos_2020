d43b932051386b32224dadafd77e2f2692d6acb9
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-09-09 22:45:24 -0300

    Update 8.5.py

--- pense_python/8.5.py
+++ pense_python/8.5.py
@@ -1,42 +1,6 @@
-def minuscula(posicao):
-    return ord(posicao) >= 97 and ord(posicao) <= 122
-
-
-def maiuscula(posicao):
-    return ord(posicao) >= 65 and ord(posicao) <= 90
-
-
-def rotate_world(string, rotacao):
-    i = 0
-    while i < len(string):
-        posicao = string[i]
-        codigo = ord(posicao) + rotacao
-
-        if minuscula(posicao):
-            if codigo > 122:
-                novo_codigo = codigo - 26
-            elif codigo < 97:
-                novo_codigo = codigo + 26
-            else:
-                novo_codigo = codigo
-
-        elif maiuscula(posicao):
-            if codigo > 90:
-                novo_codigo = codigo - 26
-            elif codigo < 65:
-                novo_codigo = codigo + 26
-            else:
-                novo_codigo = codigo
-
-        nova_string = chr(novo_codigo)
-        print(nova_string, end='')
-        
-        i += 1
-
-
 def main():
     print('>>> Que palavra você quer rotacionar?')
-    string = str(input())
+    string = input()
     print('>>> Qual valor?')
     rotacao = int(input())
 
@@ -45,7 +9,54 @@ def main():
     from time import sleep
     sleep(2)
     print('A nova palavra é:')
-    rotate_world(string, rotacao)
+    print(rotate_world(string, rotacao))
+
+
+def rotate_world(string, rotacao):
+    i = 0
+    texto = ''
+
+    while i < len(string):
+        posicao = string[i]
+        codigo = ord(posicao) + rotacao
+
+        if letra(posicao):
+            if minuscula(posicao):
+                if codigo > 122:
+                    novo_codigo = codigo - 26
+                elif codigo < 97:
+                    novo_codigo = codigo + 26
+                else:
+                    novo_codigo = codigo
+            elif maiuscula(posicao):
+                if codigo > 90:
+                    novo_codigo = codigo - 26
+                elif codigo < 65:
+                    novo_codigo = codigo + 26
+                else:
+                    novo_codigo = codigo
+
+            nova_string = chr(novo_codigo)
+        else:
+            nova_string = posicao
+        
+        texto += nova_string    
+
+        i += 1
+    
+    return texto
+
+
+def letra(posicao):
+    return minuscula(posicao) or maiuscula(posicao)
+
+
+def minuscula(posicao):
+    return ord(posicao) >= 97 and ord(posicao) <= 122
+
+
+def maiuscula(posicao):
+    return ord(posicao) >= 65 and ord(posicao) <= 90
 
 
 main()
